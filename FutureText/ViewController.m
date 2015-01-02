18f916de02a5530fc1c851a91630d08d69bb9f40
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2015-01-02 08:44:17 -0500

    addded  <MFMailComposeViewControllerDelegate>

--- FutureText/ViewController.m
+++ FutureText/ViewController.m
@@ -7,12 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface ViewController ()
+@interface ViewController ()  <MFMessageComposeViewControllerDelegate>
 
 @end
 
-@implementation ViewController
+@implementation ViewController 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,4 +25,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sendSMS:(UIButton *)sender {
+    MFMessageComposeViewController *SMSController = [[MFMessageComposeViewController alloc] init];
+    
+    if ([MFMessageComposeViewController canSendText]) {
+
+//        SMSController.recipients = @[@"8132709947"];
+        SMSController.body = @"whatsup";
+        SMSController.messageComposeDelegate = self;
+
+        
+        [self presentViewController:SMSController animated:YES completion:nil];
+        
+        
+        //[self dismissViewControllerAnimated:YES completion:nil];
+        
+    }else {
+        NSLog(@"Can't send message");
+    }
+    
+    
+    
+}
 @end
