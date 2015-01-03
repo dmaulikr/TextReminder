a9071ba8543d40233522f9b5cadb3bd0e1c2f295
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2015-01-03 11:51:45 -0500

    Added UIAlert that displays a message to the user if he hasn't turned on local notifications. Everything is working

--- FutureText/ViewController.m
+++ FutureText/ViewController.m
@@ -53,5 +53,39 @@
     if (result == MessageComposeResultFailed) NSLog(@"Message Sending Failed");
     if (result == MessageComposeResultSent) NSLog(@"Message Sent! Yay!");
 }
+
+-(void) viewDidAppear:(BOOL)animated
+{
+    // Checking if UserNotifications (Alerts) are enabled by the user
+    UIUserNotificationSettings *currentNotificationSettings = [[UIApplication sharedApplication] currentUserNotificationSettings];
+    
+    // Display notification to the user to Enable User Notifications
+    if (currentNotificationSettings.types == UIUserNotificationTypeNone) {
         
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Important"
+               message:@"In order for you to receive reminders about your messages you need to allow this app to do so. Please go to Settings > Notifications > FutureText and allow notifications."
+        preferredStyle:UIAlertControllerStyleAlert];
+        
+        // Dismiss action
+        UIAlertAction *dismissAction = [UIAlertAction actionWithTitle:@"Dismiss"
+                                    style:UIAlertActionStyleDefault
+                                  handler:^(UIAlertAction * action) {}];
+        [alert addAction:dismissAction];
+        
+        // Go to Settings action
+        UIAlertAction *goToSettingsAction = [UIAlertAction actionWithTitle:@"Go To Settings Now"
+             style:UIAlertActionStyleDefault
+           handler:^(UIAlertAction *action) {
+               
+               // Opens settings
+               [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+               
+           }];
+        [alert addAction:goToSettingsAction];
+
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
+
 @end
