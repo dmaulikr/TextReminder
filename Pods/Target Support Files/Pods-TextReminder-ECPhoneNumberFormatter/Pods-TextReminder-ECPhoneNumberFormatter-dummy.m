a51743592815db964cf4f115494256897f238afb
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2015-04-03 08:40:19 -0400

    Updated cocoa pods

---
+++ Pods/Target Support Files/Pods-TextReminder-ECPhoneNumberFormatter/Pods-TextReminder-ECPhoneNumberFormatter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TextReminder_ECPhoneNumberFormatter : NSObject
+@end
+@implementation PodsDummy_Pods_TextReminder_ECPhoneNumberFormatter
+@end
